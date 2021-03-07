fe8a61240f2b1821e923a202ea722067bfa22b34
Ephraim Benyaminov <efraimbinyaminov16@gmail.com>
2021-03-07 22:10:53 +0200

    All sections complete

---
+++ Training/RunSmart/src/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    dots: true,
+                    arrow: false
+                }
+            }
+        ],
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function() {
+        $(this)
+          .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog__item-content').eq(i).toggleClass('catalog__item-content--active');
+                $('.catalog__item-info').eq(i).toggleClass('catalog__item-info--active');
+            });
+        });
+    };
+
+    toggleSlide('.catalog__item-content');
+    toggleSlide('.catalog__item-info')
+});
